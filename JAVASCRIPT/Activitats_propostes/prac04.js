c5938d2126810474be5d55b4a4acaa8aa6630a03
AleixosFernando <fernandoaleixos@iesmre.com>
2022-03-15 13:22:14 +0100

    15-03-2022

--- JAVASCRIPT/Activitats_propostes/prac04.js
+++ JAVASCRIPT/Activitats_propostes/prac04.js
@@ -2,10 +2,10 @@ var edat = prompt("Introdueix la teua edat");
 
 if(Number(edat) == edat) {
     if(edat >= 18 && edat <= 100) {
-        document.write("Pots conduir.");
+        document.write("Puedes conducir.");
     } else {
-        document.write("No pots conduir");
+        document.write("No puedes conducir");
     }
 } else {
-    document.write(edat + " no és un nombre!!");
+    document.write(edat + " no és un número!!");
 }
