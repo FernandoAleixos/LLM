f5a92d131627a0c78569705c2b93af7201844d52
AleixosFernando <fernandoaleixos@iesmre.com>
2022-04-05 11:09:25 +0200

    menu JS

---
+++ JAVASCRIPT/Activitats_propostes/menu.js
@@ -0,0 +1,15 @@
+function suma(valor1, valor2) {
+    return valor1 + valor2;
+}
+
+function resta(num1, num2) {
+    return num1 - num2;
+}
+
+function multiplicacio(num1, num2) {
+    return num1 * num2;
+}
+
+function divisio(num1, num2) {
+    return num1 / num2;
+}
