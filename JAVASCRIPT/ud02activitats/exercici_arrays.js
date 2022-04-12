4ba50a2dc9ead831955a05785672a93d14cbdda3
AleixosFernando <fernandoaleixos@iesmre.com>
2022-04-12 11:10:44 +0200

    12-04-2022

---
+++ JAVASCRIPT/ud02activitats/exercici_arrays.js
@@ -0,0 +1,14 @@
+function reiniciar() {
+    location.reload();
+}
+
+function carregaExemple() {
+    let cadena = "";
+
+    for (let i = 1; i <= 5; i++) {
+        cadena += Math.floor((Math.random() * 100) + 1);
+
+        cadena = cadena.substring(0, cadena.length - 1);
+        document.getElementById("text");
+    }
+}
