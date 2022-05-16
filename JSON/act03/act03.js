68297e150ad53e81b8be5ff603b754cfc31d467f
AleixosFernando <fernandoaleixos@iesmre.com>
2022-05-16 09:46:14 +0200

    16/05/2022

---
+++ JSON/act03/act03.js
@@ -0,0 +1,10 @@
+const user = {
+    "name": "artur",
+    "lastname": "blas"
+}
+
+//objeto javascript en consola
+console.log (user);
+
+//objeto json en consola
+console.log(JSON.stringify(user));
